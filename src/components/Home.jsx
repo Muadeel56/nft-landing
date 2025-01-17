ada054902394b563558152db06e9069d8a62b657
Adeel591 <muadeel591@gmail.com>
2025-01-17 16:00:32 +0000

    Create home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import home from '../assets/home.png'
 
 function Home() {
   return (
-    <div></div>
+    <div className="home">
+      <div className="container">
+        <div className="content">
+          <p className="sub-title">Launching Soon</p>
+          <h1 className="title">An NFT like no other</h1>
+          <p className="description">Don't miss out on the release of 
+            your new NFT. Sign up below to recieve updates when we go 
+            live.
+          </p>
+          <button>Sign Up</button>
+        </div>
+        <div className="image-container">
+          <div className="image">
+            <img src={home} alt="image" />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
